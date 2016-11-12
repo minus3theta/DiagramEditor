5d5b094a1b52f17e3a8c87660a369d86284b3cda
minus3theta <yone_j_synthesis@yahoo.co.jp>
2016-11-13 02:09:02 +0900

    Implement edge drawing

--- js/body.js
+++ js/body.js
@@ -3,6 +3,8 @@ var cWidth;
 var cHeight;
 var outXyPic;
 var dgm;
+var radius;
+var fontHeight;
 
 class EdgeStyle {
     constructor(s) {
@@ -24,35 +26,45 @@ class EdgeLabel {
 }
 
 class Edge {
-    constructor(dx, dy, label, style) {
-        this.dx = dx;           // Number
-        this.dy = dy;           // Number
+    constructor(x, y, stepX, stepY, label, style) {
+        this.stepX = stepX;     // Number
+        this.stepY = stepY;     // Number
         this.label = label;     // EdgeLabel
         this.style = style;     // EdgeStyle
+        var dx = stepX * dgm.dx;
+        var dy = stepY * dgm.dy;
+        var d = radius / Math.sqrt(dx * dx + dy * dy);
+        this.x0 = x + d * dx;
+        this.y0 = y + d * dy;
+        this.x1 = x + (1 - d) * stepX * dgm.dx;
+        this.y1 = y + (1 - d) * stepY * dgm.dy;
     }
     toXyPic() {
         var dir = "";
-        if(this.dx >= 0) {
-            dir += "r".repeat(this.dx);
+        if(this.stepX >= 0) {
+            dir += "r".repeat(this.stepX);
         } else {
-            dir += "l".repeat(-this.dx);
+            dir += "l".repeat(-this.stepX);
         }
-        if(this.dy >= 0) {
-            dir += "d".repeat(this.dy);
+        if(this.stepY >= 0) {
+            dir += "d".repeat(this.stepY);
         } else {
-            dir += "u".repeat(-this.dy);
+            dir += "u".repeat(-this.stepY);
         }
         return "\\ar[" + dir + "]" + this.style.toXyPic() + this.label.toXyPic();
     }
-    draw(c, x, y, pitch) {
-        return this;
+    draw(c) {
+        c.beginPath();
+        c.moveTo(this.x0, this.y0);
+        c.lineTo(this.x1, this.y1);
+        c.stroke();
     }
 }
 
 class Node {
-    constructor(i, j, label) {
-        this.i = i;             // Number
-        this.j = j;             // Number
+    constructor(x, y, label) {
+        this.x = x;             // Number
+        this.y = y;             // Number
         this.label = label;     // String
         this.edges = [];        // [Edge]
     }
@@ -63,13 +75,18 @@ class Node {
         }
         return s;
     }
-    draw(c, pitch, marginX, marginY) {
+    addEdge() {
+        this.edges.push(
+            new Edge(this.x,this.y,1,1,new EdgeLabel("f", "^"), new EdgeStyle("")));
+    }
+    draw(c) {
         var l = c.measureText(this.label).width;
-        var x = this.i * pitch + marginX;
-        var y = this.j * pitch + marginY;
-        c.fillText(this.label, x - l / 2, y);
+        c.fillText(this.label, this.x - l / 2, this.y);
+        var margin = fontHeight / 2;
+        c.strokeRect(this.x - l / 2 - margin, this.y - fontHeight / 2 - margin,
+                     l + margin * 2, fontHeight + margin * 2);
         this.edges.forEach(function(edge) {
-            edge.draw(c, x, y, pitch);
+            edge.draw(c);
         });
     }
 }
@@ -79,10 +96,13 @@ class Diagram {
         this.w = w;             // Number
         this.h = h;             // Number
         this.table = new Array(h); // [[Node]]
+        this.dx = cWidth / w;
+        this.dy = cHeight / h;
         for(var i=0; i<h; ++i) {
             this.table[i] = new Array(w);
             for(var j=0; j<w; ++j) {
-                this.table[i][j] = new Node(j, i, "("+i*10000+", "+j+")");
+                this.table[i][j] =
+                    new Node(this.jtox(j), this.itoy(i), "("+i+", "+j+")");
             }
         }
     }
@@ -105,21 +125,25 @@ class Diagram {
         c.fillStyle = "black";
         for(var i=0; i<this.h; ++i) {
             for(var j=0; j<this.w; ++j) {
-                this.table[i][j].draw(c, 200, 50, 50);
+                this.table[i][j].draw(c);
             }
         }
     }
-    select(elem) {
-        elem.innerHTML;
+    jtox(j) {
+        return (j+0.5) * this.dx;
+    }
+    itoy(i) {
+        return (i+0.5) * this.dy;
     }
 }
 
 function reset() {
     var width = Number(document.getElementById("dgmWidth").value);
     var height = Number(document.getElementById("dgmHeight").value);
+    radius = fontHeight = (width < 5 && height < 6) ? 24 : 18;
+    canvas.font = fontHeight + "px 'Times New Roman'";
     dgm = new Diagram(width, height);
-    dgm.table[0][0].edges.push(
-        new Edge(1,1,new EdgeLabel("f", "^"), new EdgeStyle("")));
+    dgm.table[0][0].addEdge();
     outXyPic.innerHTML = "\\xymatrix{\n"+ dgm.toXyPic() +"}";
     dgm.draw(canvas);
 }
@@ -132,7 +156,7 @@ window.addEventListener("load",function(eve){
     cWidth = c.width;
     cHeight = c.height;
     canvas = c.getContext("2d");
-    canvas.font = "24px 'Times New Roman'";
+    canvas.textBaseline = "middle";
     outXyPic = document.getElementById("outXyPicContents");
     reset();
 },false);
