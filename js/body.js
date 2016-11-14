f3eacee34e39b428ff38535cfa85dd88e9f40403
minus3theta <yone_j_synthesis@yahoo.co.jp>
2016-11-14 12:17:30 +0900

    Implement edge drawing

--- js/body.js
+++ js/body.js
@@ -1,107 +1,3 @@
-class EdgeStyle {
-    constructor(s) {
-        this.s = s;             // String
-    }
-    toXyPic() {
-        return this.s;
-    }
-}
-
-class EdgeLabel {
-    constructor(str, pos) {
-        this.str = str;         // String
-        this.pos = pos;         // String
-    }
-    toXyPic() {
-        return this.pos + "-{" + this.str + "}";
-    }
-    toString() {
-        return this.str;
-    }
-}
-
-class Edge {
-    constructor(x, y, stepX, stepY, label, style) {
-        this.stepX = stepX;     // Number
-        this.stepY = stepY;     // Number
-        this.label = label;     // EdgeLabel
-        this.style = style;     // EdgeStyle
-        var dx = stepX * dgm.dx;
-        var dy = stepY * dgm.dy;
-        this.theta = Math.atan2(dy, dx);
-        this.x0 = x + radius * Math.cos(this.theta);
-        this.y0 = y + radius * Math.sin(this.theta);
-        this.x1 = x + dx - radius * Math.cos(this.theta);
-        this.y1 = y + dy - radius * Math.sin(this.theta);
-    }
-    toXyPic() {
-        var dir = "";
-        if(this.stepX >= 0) {
-            dir += "r".repeat(this.stepX);
-        } else {
-            dir += "l".repeat(-this.stepX);
-        }
-        if(this.stepY >= 0) {
-            dir += "d".repeat(this.stepY);
-        } else {
-            dir += "u".repeat(-this.stepY);
-        }
-        return "\\ar[" + dir + "]" + this.style.toXyPic() + this.label.toXyPic();
-    }
-    draw(c) {
-        var l = 20;
-        var phi = 0.3;
-        c.beginPath();
-        c.moveTo(this.x0, this.y0);
-        c.lineTo(this.x1, this.y1);
-        c.stroke();
-        c.beginPath();
-        c.moveTo(this.x1, this.y1);
-        c.lineTo(this.x1 - l * Math.cos(this.theta + phi),
-                 this.y1 - l * Math.sin(this.theta + phi));
-        c.stroke();
-        c.moveTo(this.x1, this.y1);
-        c.lineTo(this.x1 - l * Math.cos(this.theta - phi),
-                 this.y1 - l * Math.sin(this.theta - phi));
-        c.stroke();
-        c.fillText(this.label, (this.x0 + this.x1) / 2, (this.y0 + this.y1) / 2);
-    }
-}
-
-class Node {
-    constructor(x, y, label) {
-        this.x = x;             // Number
-        this.y = y;             // Number
-        this.label = label;     // String
-        this.edges = [];        // [Edge]
-    }
-    toXyPic() {
-        var s = this.label;
-        for(var i=0; i<this.edges.length; ++i) {
-            s += " " + this.edges[i].toXyPic();
-        }
-        return s;
-    }
-    addEdge() {
-        this.edges.push(
-            new Edge(this.x,this.y,1,1,new EdgeLabel("f", "^"), new EdgeStyle("")));
-    }
-    draw(c) {
-        var l = c.measureText(this.label).width;
-        c.fillText(this.label, this.x - l / 2, this.y);
-        var margin = fontHeight / 2;
-        c.strokeRect(this.x - l / 2 - margin, this.y - fontHeight / 2 - margin,
-                     l + margin * 2, fontHeight + margin * 2);
-        this.edges.forEach(function(edge) {
-            edge.draw(c);
-        });
-    }
-    select() {
-        conf.innerHTML =
-            "<input type='text' id='obj_label' onchange='update();'></input>";
-    }
-}
-
 phina.globalize();
 
 phina.main(function() {
@@ -110,7 +6,7 @@ phina.main(function() {
         domElement: document.getElementById("world"),
         width: 600,
         height: 600,
-        fit: false
+        fit: false,
     });
     app.run();
 });
@@ -152,6 +48,7 @@ phina.define("MainScene", {
         }
         this.dgm = Diagram(this.dgmWidth, this.dgmHeight,
                            this.gridX, this.gridY).addChildTo(this);
+        this.dgm.objs[1][1].addEdge(this.dgm.objs[0][0], "f", "^", "");
     },
     write: function() {
         outXyPic.innerHTML = "\\xymatrix{\n"+ this.dgm.toXyPic() +"}";
@@ -159,17 +56,25 @@ phina.define("MainScene", {
 });
 
 phina.define("Diagram", {
-    superClass: "DisplayElement",
+    superClass: "Shape",
     init: function(w, h, gridX, gridY) {
-        this.superInit();
+        this.superInit({
+            originX: 0,
+            originY: 0,
+            width: 600,
+            height: 600,
+            backgroundColor: "white",
+            stroke: "black",
+            strokeWidth: 8,
+        });
         this.w = w;
         this.h = h;
-        this.objArray = new Array(h);
+        this.objs = new Array(h);
         for(var i=0; i<h; ++i) {
-            this.objArray[i] = new Array(w);
+            this.objs[i] = new Array(w);
             for(var j=0; j<w; ++j) {
-                this.objArray[i][j] =
-                    Obj("("+j+","+i+")",
+                this.objs[i][j] =
+                    Obj("("+j+","+i+")", i, j,
                         gridX.span(j) + gridX.unit() / 2,
                         gridY.span(i) + gridY.unit() / 2)
                     .addChildTo(this);
@@ -180,7 +85,7 @@ phina.define("Diagram", {
         var s = "";
         for(var i=0; i<this.h; ++i) {
             for(var j=0; j<this.w; ++j) {
-                s += this.objArray[i][j].toXyPic();
+                s += this.objs[i][j].toXyPic();
                 if (j != this.w-1) {
                     s += " &amp; ";
                 }
@@ -193,7 +98,7 @@ phina.define("Diagram", {
 
 phina.define("Obj", {
     superClass: "RectangleShape",
-    init: function(label, x, y) {
+    init: function(text, i, j, x, y) {
         this.superInit({
             width: 100,
             height: 50,
@@ -203,30 +108,70 @@ phina.define("Obj", {
             x: x,
             y: y,
         });
-        this.label = ObjLabel(label).addChildTo(this);
+        this.i = i;
+        this.j = j;
+        this.label = Label(text).addChildTo(this);
+        this.label.stroke = "black";
         this.width = this.label.calcCanvasWidth();
         this.edges = [];
     },
     toXyPic: function() {
-        return this.label.toXyPic();
+        var s = this.label.text;
+        for(var i=0; i<this.edges.length; ++i) {
+            s += this.edges[i].toXyPic();
+        }
+        return s;
     },
-});
-
-phina.define("ObjLabel", {
-    superClass: "Label",
-    init: function(str) {
-        this.superInit(str);
-        this.str = str;
-        this.stroke = "black";
-    },
-    toXyPic: function() {
-        return this.str;
+    addEdge: function(dst, text, pos, style) {
+        var e = Edge(this, dst, text, pos, style);
+        this.edges.push(e);
+        e.addChildTo(this);
     },
 });
 
 phina.define("Edge", {
     superClass: "DisplayElement",
-    init: function() {
+    init: function(src, dst, text, pos, style) {
         this.superInit();
+        var dx = dst.x - src.x;
+        var dy = dst.y - src.y;
+        var r = 30;
+        this.theta = Math.atan2(dy, dx);
+        this.x0 = r * Math.cos(this.theta);
+        this.y0 = r * Math.sin(this.theta);
+        this.x1 = dx - r * Math.cos(this.theta);;
+        this.y1 = dy - r * Math.sin(this.theta);;
+        this.stepI = dst.i - src.i;
+        this.stepJ = dst.j - src.j;
+        this.frame = RectangleShape({
+            width: 0,
+            height: 50,
+            fill: null,
+            stroke: "gray",
+            cornerRadius: 4,
+            x: dx / 2,
+            y: dy / 2,
+        }).addChildTo(this);
+        this.label = Label(text).addChildTo(this.frame);
+        this.frame.width = this.label.calcCanvasWidth();
+        this.pos = pos;
+        this.style = style;
+    },
+    toXyPic: function() {
+        var dir = "";
+        if(this.stepI >= 0) {
+            dir += "d".repeat(this.stepI);
+        } else {
+            dir += "u".repeat(-this.stepI);
+        }
+        if(this.stepJ >= 0) {
+            dir += "r".repeat(this.stepJ);
+        } else {
+            dir += "l".repeat(-this.stepJ);
+        }
+        return " \\ar[" + dir + "]" + this.pos + "-{" + this.label.text + "}";
+    },
+    draw: function(canvas) {
+        canvas.drawArrow(this.x0, this.y0, this.x1, this.y1, 10);
     },
 });
